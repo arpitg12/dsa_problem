47d4ac4890533dc76574b208086873b66587e43b
Arpit Agrawal <121594026+arpitg12@users.noreply.github.com>
2024-02-27 09:15:40 +0530

    Add files via upload
    Given an array of size n such that each element contains either a 'P' for policeman or a 'T' for thief. Find the maximum number of thieves that can be caught by the police. 
    Keep in mind the following conditions :
    
    Each policeman can catch only one thief.
    A policeman cannot catch a thief who is more than K units away from him.

---
+++ Police and Thieves.cpp
@@ -0,0 +1,33 @@
+class Solution{
+    public:
+    int catchThieves(char arr[], int n, int k) 
+    {
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]=='P'){
+                bool flag=true;
+                int run=max(0,i-k);
+                for(int j=run;j<i;j++){
+                    if(arr[j]=='T'){
+                        cnt++;
+                        arr[j]='C';
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag){
+                    int run=min(n-1,i+k);
+                    for(int j=i+1;j<=run;j++){
+                         if(arr[j]=='T'){
+                        cnt++;
+                        arr[j]='C';
+                        break;
+                    }
+                    }
+                }
+            }
+        }
+        return cnt;
+        // Code here 
+    }
+};
