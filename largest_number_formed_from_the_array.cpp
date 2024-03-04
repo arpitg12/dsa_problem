577616e1c05c8c469053d0da11a40cf06940f50d
Arpit Agrawal <121594026+arpitg12@users.noreply.github.com>
2024-03-04 19:11:02 +0530

    Update

---
+++ largest_number_formed_from_the_array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+static bool cmp(string a, string b){
+	    if(a+b>b+a){
+	        return true;
+	    }
+	    return false;
+	}
+	string printLargest(int n,
+	vector<string> &arr) {
+	    sort(arr.begin(),arr.end(),cmp);
+	    string s="";
+	    for(int i=0;i<n;i++){
+	        s=s+arr[i];
+	    }
+	    return s;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(n, arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
